c5c7836a7d494ad23ed3a9c426306e186df8497d
Faisal Pathan <46352078+faisal-pathan@users.noreply.github.com>
2021-08-02 09:57:06 +0530

    Latest Changes

--- index.php
+++ index.php
@@ -2,45 +2,101 @@
 <head>
     <title>Retrive Data</title>
 </head>
+<style>
+    table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+     th, td {
+        padding: 10px;
+         font-size: 20px;
+    }
+    strong {
+        color: red;
+    }
+    em {
+        color: black;
+    }
+    fa {
+        padding: 20px;
+    }
+</style>
+
 <body>
+
 <center>
-    <h2>Enter User Name</h2>
-    <input type="text" id="Parcel" required="required"><br>
-    <button type="button" onclick="getdata();">GET</button><br>
+<table border="1", style="width: 10%">
+    <tr>
+        <th>BOX ID</th>
+        <th>Length</th>
+        <th>Breadth</th>
+        <th>Height</th>
+        <th>Remark</th>
+    </tr>
+
+    <tr>
+        <th><p id="id"></p></th>
+        <td><fa><em id="length"></em><strong id="wronglength"></strong></fa></td>
+        <td><fa><em id="breadth"></em><strong id="wrongbreadth"></strong></fa></td>
+        <td><fa><em id="height"></em><strong id="wrongheight"></strong></fa></td>
+        <td><em id="remark"></em><strong id="wrongremark"></strong></td>
+    </tr>
+
+</table>
+
+
 </center>
+<br>
 <center>
-    <p>Length: <strong id="length"></strong></p>
-    <p>Breadth: <strong id="breadth"></strong></p>
-    <p>Height: <strong id="height"></strong></p>
+        <button type="button" onclick="getdata();">GET</button><br>
 </center>
 
 
 
 <!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/8.7.0/firebase.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.7.1/firebase.js"></script>
 
 <!-- TODO: Add SDKs for Firebase products that you want to use
      https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="https://www.gstatic.com/firebasejs/8.7.0/firebase-analytics.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.7.1/firebase-analytics.js"></script>
 
 <script>
     // Your web app's Firebase configuration
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
     var firebaseConfig = {
-        apiKey: "AIzaSyCCVL5a5gifa9lJ9JSW3JCR84r7ur8gu9w",
-        authDomain: "fir-senddata-73949.firebaseapp.com",
-        databaseURL: "https://fir-senddata-73949-default-rtdb.firebaseio.com",
-        projectId: "fir-senddata-73949",
-        storageBucket: "fir-senddata-73949.appspot.com",
-        messagingSenderId: "1055015658230",
-        appId: "1:1055015658230:web:89c1e2ceb7dca7289b42d7",
-        measurementId: "G-Q1YMF10M2K"
+        apiKey: "AIzaSyBc1smFAzN2aMV7mWDkjstmgegN_2RJSas",
+        authDomain: "fir-featurelearn.firebaseapp.com",
+        databaseURL: "https://fir-featurelearn-default-rtdb.firebaseio.com",
+        projectId: "fir-featurelearn",
+        storageBucket: "fir-featurelearn.appspot.com",
+        messagingSenderId: "1027255264454",
+        appId: "1:1027255264454:web:15ac64bf07e9239a8823c7",
+        measurementId: "G-6XXVPF8PSS"
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
-
-    //firebase.analytics();
+    firebase.analytics();
 </script>
+<script>
+    var connectedRef = firebase.database().ref(".info/connected");
+    connectedRef.on("value", (snap) => {
+        if (snap.val() === true) {
+            console.log("connected");
+            window.alert("Connected to server");
+        } else {
+            console.log("not connected");
+            window.alert("Disconnected from server");
+        }
+    });
+</script>
+<script>
+    var presenceRef = firebase.database().ref("disconnectmessage");
+    // Write a string when this client loses connection
+    presenceRef.onDisconnect().set("I disconnected!");
+</script>
+
 <script type="text/javascript" src="js/Read.js"></script>
+
+
 </body>
 </html>
--- js/Read.js
+++ js/Read.js
@@ -1,16 +1,52 @@
 function  getdata(){
-    var Parcel = document.getElementById("Parcel").value;
 
-    firebase.database().ref('Parcel/'+Parcel).once('value').then(function (snapshot) {
+    firebase.database().ref('Parcel/').once('value').then(function (snapshot) {
 
-       var length=snapshot.val().Length;
-       var height=snapshot.val().Height;
-       var breadth=snapshot.val().Breadth;
+        var length=snapshot.val().Length;
+        var height=snapshot.val().Height;
+        var breadth=snapshot.val().Width;
+        var id=snapshot.val().ID;
+        var remark = "Data in Range";
+
+        var wronglength=snapshot.val().Length;
+        var wrongbreadth=snapshot.val().Width;
+        var wrongheight=snapshot.val().Height;
+        var wrongremark = "Range Exceed";
+
+        document.getElementById("id").innerHTML=id;
+        var limit = 100;
+
+        if(length > limit){
+            document.getElementById("wronglength").innerHTML=wronglength;
+            document.getElementById("wrongremark").innerHTML = wrongremark;
+        }
+        else {
+            document.getElementById("length").innerHTML=length;
+            document.getElementById("remark").innerHTML = remark;
+        }
+
+        if(breadth > limit){
+            document.getElementById("wrongbreadth").innerHTML=wrongbreadth;
+            document.getElementById("wrongremark").innerHTML = wrongremark;
+        }
+        else {
+            document.getElementById("breadth").innerHTML=breadth;
+            document.getElementById("remark").innerHTML = remark;
+        }
+
+        if(height > limit){
+            document.getElementById("wrongheight").innerHTML=wrongheight;
+            document.getElementById("wrongremark").innerHTML = wrongremark;
+        }
+        else {
+            document.getElementById("height").innerHTML=height;
+            document.getElementById("remark").innerHTML = remark;
+        }
 
 
-        document.getElementById("length").innerHTML=length;
-        document.getElementById("height").innerHTML=height;
-        document.getElementById("breadth").innerHTML=breadth;
+
+        //document.getElementById("height").innerHTML=height;
+        //document.getElementById("breadth").innerHTML=breadth;
 
     })
 }
